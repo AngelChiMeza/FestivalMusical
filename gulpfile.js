96d2c0dca13843bc7afe1097fefe544460b69b1f
AngelChiMeza <angel.chi@uimqroo.edu.mx>
2023-07-11 16:32:51 -0500

    CambiosAngel11072023-1632hr

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,7 @@ const plumber = require("gulp-plumber");
 const cache = require("gulp-cache");
 const imagemin = require("gulp-imagemin");
 const webp = require("gulp-webp");
+const avif = require("gulp-avif");
 
 function css(done) {
   src("src/scss/**/*.scss") // Identificar el archivo de SASS
@@ -34,13 +35,16 @@ function versionWebp(done) {
     .pipe(dest("build/img")); // Destino
   done();
 }
-// function versionWebp(done) {
-//   const opciones = {
-//     quality: 50,
-//   };
-//   src("src/img/**/*.{png,jpg}").pipe(webp(opciones)).pipe(dest("build/img"));
-//   done();
-// }
+function versionAvif(done) {
+  const opciones = {
+    quality: 50,
+  };
+  src("src/img/**/*.{png,jpg}")
+    .pipe(avif(opciones)) // Webp
+    .pipe(dest("build/img")); // Destino
+  done();
+}
+
 function dev(done) {
   watch("src/scss/**/*.scss", css);
   done();
@@ -48,4 +52,5 @@ function dev(done) {
 exports.css = css;
 exports.imagenes = imagenes;
 exports.versionWebp = versionWebp;
-exports.dev = parallel(imagenes, versionWebp, dev);
+exports.versionAvif = versionAvif;
+exports.dev = parallel(imagenes, versionWebp, versionAvif, dev);
