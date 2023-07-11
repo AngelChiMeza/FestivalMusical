77d6750f4e21452bda5f73ff4d34faa857e59056
AngelChiMeza <angel.chi@uimqroo.edu.mx>
2023-07-11 15:20:24 -0500

    actualizacion al 11072023

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,9 @@
-const { src, dest, watch } = require("gulp");
+const { src, dest, watch, parallel } = require("gulp");
+//CSS
 const sass = require("gulp-sass")(require("sass"));
 const plumber = require("gulp-plumber");
+// Imagenes
+const webp = require("gulp-webp");
 
 function css(done) {
   src("src/scss/**/*.scss") // Identificar el archivo de SASS
@@ -10,9 +13,27 @@ function css(done) {
 
   done(); // Callback que avisa a gulp cuando llegamos al final
 }
+
+function versionWebp(done) {
+  const opciones = {
+    quality: 50,
+  };
+  src("src/img/**/*.{png,jpg}")
+    .pipe(webp(opciones)) // Webp
+    .pipe(dest("build/img")); // Destino
+  done();
+}
+// function versionWebp(done) {
+//   const opciones = {
+//     quality: 50,
+//   };
+//   src("src/img/**/*.{png,jpg}").pipe(webp(opciones)).pipe(dest("build/img"));
+//   done();
+// }
 function dev(done) {
   watch("src/scss/**/*.scss", css);
   done();
 }
 exports.css = css;
-exports.dev = dev;
+exports.versionWebp = versionWebp;
+exports.dev = parallel(versionWebp, dev);
