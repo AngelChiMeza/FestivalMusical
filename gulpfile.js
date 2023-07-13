38e80865120c19332cae49f778cefef30942517b
AngelChiMeza <angel.chi@uimqroo.edu.mx>
2023-07-12 22:14:47 -0500

    Proyecto Finalizado

--- gulpfile.js
+++ gulpfile.js
@@ -2,16 +2,27 @@ const { src, dest, watch, parallel } = require("gulp");
 //CSS
 const sass = require("gulp-sass")(require("sass"));
 const plumber = require("gulp-plumber");
+const autoprefixer = require("autoprefixer");
+const cssnano = require("cssnano");
+const postcss = require("gulp-postcss");
+const sourcemaps = require("gulp-sourcemaps");
+
 // Imagenes
 const cache = require("gulp-cache");
 const imagemin = require("gulp-imagemin");
 const webp = require("gulp-webp");
 const avif = require("gulp-avif");
 
+//Javascript
+const terser = require("gulp-terser-js");
+
 function css(done) {
   src("src/scss/**/*.scss") // Identificar el archivo de SASS
+    .pipe(sourcemaps.init())
     .pipe(plumber())
     .pipe(sass()) // Compilarlo
+    .pipe(postcss([autoprefixer(), cssnano()])) // Compilarlo
+    .pipe(sourcemaps.write("."))
     .pipe(dest("build/css")); // Almacenarla en el HD
 
   done(); // Callback que avisa a gulp cuando llegamos al final
@@ -44,13 +55,23 @@ function versionAvif(done) {
     .pipe(dest("build/img")); // Destino
   done();
 }
+function javascript(done) {
+  src("src/js/**/*.js") // Origen
+    .pipe(sourcemaps.init())
+    .pipe(terser())
+    .pipe(sourcemaps.write("."))
+    .pipe(dest("build/js")); // Destino
+  done();
+}
 
 function dev(done) {
   watch("src/scss/**/*.scss", css);
+  watch("src/js/**/*.js", javascript);
   done();
 }
 exports.css = css;
+exports.js = javascript;
 exports.imagenes = imagenes;
 exports.versionWebp = versionWebp;
 exports.versionAvif = versionAvif;
-exports.dev = parallel(imagenes, versionWebp, versionAvif, dev);
+exports.dev = parallel(imagenes, versionWebp, versionAvif, javascript, dev);
